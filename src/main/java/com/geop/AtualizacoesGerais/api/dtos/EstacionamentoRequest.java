d1b4989802f65d32744acd4a67d0a68a84997a21
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-06-17 09:21:04 -0300

    created EstacionamentoRequest

---
+++ src/main/java/com/geop/AtualizacoesGerais/api/dtos/EstacionamentoRequest.java
@@ -0,0 +1,74 @@
+package com.geop.AtualizacoesGerais.api.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.geop.AtualizacoesGerais.domain.entities.Estacionamento;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+public record EstacionamentoRequest(
+        String idVaga,
+        @NotNull
+        String localizacao,
+        @NotNull
+        String tipoVaga,
+        String sensorId,
+        String cameraId,
+        String placaVeiculo,
+        @NotNull
+        Boolean ocupada,
+        Integer tempoOcupacaoAtual,
+        Integer quantidadeTrocasDia,
+        Integer vagasOcupadas,
+        Integer vagasLivres,
+        String horarioPico,
+        Integer tempoTotalLivre,
+        Boolean vagasSubutilizadas,
+        Boolean vagasCriticas,
+        Double taxaRotatividade,
+        Double taxaOcupacao,
+        String diaMaiorMovimento,
+        Integer mediaDiariaEntradaVeiculos,
+        String previsaoFluxoProximoHorario,
+        Integer totalVagas,
+        String eventoIrregularDetectado,
+        LocalDateTime dataHoraEvento,
+        Double percentualUsoPorVaga,
+        Integer tempoMedioOcupacao,
+        Integer horarioDePicoMedia
+) {
+    public static Estacionamento toEntidade(EstacionamentoRequest estacionamentoRequest){
+        if (estacionamentoRequest==null){
+            return null;
+        }
+        return new Estacionamento(
+                estacionamentoRequest.idVaga(),
+                estacionamentoRequest.localizacao(),
+                estacionamentoRequest.tipoVaga(),
+                estacionamentoRequest.sensorId(),
+                estacionamentoRequest.cameraId(),
+                estacionamentoRequest.placaVeiculo(),
+                estacionamentoRequest.ocupada(),
+                estacionamentoRequest.tempoOcupacaoAtual(),
+                estacionamentoRequest.quantidadeTrocasDia(),
+                estacionamentoRequest.vagasOcupadas(),
+                estacionamentoRequest.vagasLivres(),
+                estacionamentoRequest.horarioPico(),
+                estacionamentoRequest.tempoTotalLivre(),
+                estacionamentoRequest.vagasSubutilizadas(),
+                estacionamentoRequest.vagasCriticas(),
+                estacionamentoRequest.taxaRotatividade(),
+                estacionamentoRequest.taxaOcupacao(),
+                estacionamentoRequest.diaMaiorMovimento(),
+                estacionamentoRequest.mediaDiariaEntradaVeiculos(),
+                estacionamentoRequest.previsaoFluxoProximoHorario(),
+                estacionamentoRequest.totalVagas(),
+                estacionamentoRequest.eventoIrregularDetectado(),
+                estacionamentoRequest.dataHoraEvento(),
+                estacionamentoRequest.percentualUsoPorVaga(),
+                estacionamentoRequest.tempoMedioOcupacao(),
+                estacionamentoRequest.horarioDePicoMedia()
+        );
+    }
+}
