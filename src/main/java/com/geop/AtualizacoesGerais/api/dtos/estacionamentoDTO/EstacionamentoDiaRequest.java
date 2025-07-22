cb7f5cc8de35b5a0d90e7de651cf8966c56368b7
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-07-22 17:03:33 -0300

    update EstacionamentoDiaRequest

--- src/main/java/com/geop/AtualizacoesGerais/api/dtos/estacionamentoDTO/EstacionamentoDiaRequest.java
+++ src/main/java/com/geop/AtualizacoesGerais/api/dtos/estacionamentoDTO/EstacionamentoDiaRequest.java
@@ -1,6 +1,39 @@
 package com.geop.AtualizacoesGerais.api.dtos.estacionamentoDTO;
 
-public record EstacionamentoDiaRequest(
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.geop.AtualizacoesGerais.domain.entities.EstacionamentoDiaDados;
 
+import java.time.LocalDateTime;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record EstacionamentoDiaRequest(
+         Boolean ocupada,
+         Integer tempoOcupacaoAtual,
+         Integer quantidadeTrocasDia,
+         Integer vagasOcupadas,
+         Integer vagasLivres,
+         String horarioPico,
+         Integer tempoTotalLivre,
+         Boolean vagasSubutilizadas,
+         Boolean vagasCriticas,
+         LocalDateTime dataHoraEvento
 ) {
+    public static EstacionamentoDiaDados toEntidade(EstacionamentoDiaRequest estacionamentoDiaRequest){
+        if (estacionamentoDiaRequest==null){
+            return null;
+        }
+        return new EstacionamentoDiaDados(
+                estacionamentoDiaRequest.ocupada,
+                estacionamentoDiaRequest.tempoOcupacaoAtual,
+                estacionamentoDiaRequest.quantidadeTrocasDia,
+                estacionamentoDiaRequest.vagasOcupadas,
+                estacionamentoDiaRequest.vagasLivres,
+                estacionamentoDiaRequest.horarioPico,
+                estacionamentoDiaRequest.tempoTotalLivre,
+                estacionamentoDiaRequest.vagasSubutilizadas,
+                estacionamentoDiaRequest.vagasCriticas,
+                estacionamentoDiaRequest.dataHoraEvento
+        );
+    }
 }
