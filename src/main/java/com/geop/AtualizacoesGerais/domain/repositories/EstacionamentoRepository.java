05a9de1736d3a959fa918fc09a73e7bdf72ce099
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-07-22 17:10:09 -0300

    update EstacionamentoRepository

--- src/main/java/com/geop/AtualizacoesGerais/domain/repositories/EstacionamentoRepository.java
+++ src/main/java/com/geop/AtualizacoesGerais/domain/repositories/EstacionamentoRepository.java
@@ -1,9 +1,10 @@
 package com.geop.AtualizacoesGerais.domain.repositories;
 
-import com.geop.AtualizacoesGerais.domain.entities.Estacionamento;
+import com.geop.AtualizacoesGerais.domain.entities.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -11,10 +12,11 @@ import java.util.UUID;
 public interface EstacionamentoRepository extends JpaRepository<Estacionamento, UUID> {
     Optional<Estacionamento>findById(UUID uuid);
     Optional<Estacionamento>findByIdVaga(String idVaga);
-    @Query("SELECT e FROM Estacionamento e WHERE DATE(e.dataHoraEvento) = CURRENT_DATE")
-    List<Estacionamento> findAllByDataHoje();
-    @Query("SELECT e FROM Estacionamento e WHERE DATE(e.dataHoraEvento) = CURRENT_DATE")
-    List<Estacionamento> findAllByDataMes();
-    @Query("SELECT e FROM Estacionamento e WHERE DATE(e.dataHoraEvento) = CURRENT_DATE")
-    List<Estacionamento> findAllByDataAno();
+
+    @Query("SELECT e FROM estacionamento_semana_dados e WHERE DATE(e.dataHoraEvento) = CURRENT_DATE")
+    List<EstacionamentoSemanaDados> findAllBySemana(LocalDateTime localDateTime);
+    @Query("SELECT e FROM estacionamento_mes_dados e WHERE DATE(e.dataHoraEvento) = CURRENT_DATE")
+    List<EstacionamentoMesDados> findAllByDataMes(LocalDateTime localDateTime);
+    @Query("SELECT e FROM estacionamento_ano_dados e WHERE DATE(e.dataHoraEvento) = CURRENT_DATE")
+    List<EstacionamentoAnoDados> findAllByDataAno(LocalDateTime localDateTime);
 }
